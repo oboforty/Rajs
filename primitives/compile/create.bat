a4a0110cf1d93bff84b11c04145444b01b324f20
r.csombordi <r.csombordi@fugro.com>
2017-09-21 11:09:52 +0200

    Added js minifier for primitives

---
+++ primitives/compile/create.bat
@@ -0,0 +1,5 @@
+:RES
+cls
+python main.py
+set /p id="Restart? " %=%
+goto:RES
---
+++ primitives/compile/main.py
@@ -0,0 +1,17 @@
+import sys
+from jsmin import jsmin
+from os import listdir
+from os.path import isfile, join
+
+content = ""
+for f in listdir("../"):
+	filename = "../" + f
+	if isfile(filename):
+		with open(filename, "r") as fh:
+			content += fh.read()
+
+print("Writing files...")
+with open('../dist/rajs.min.js', 'w') as fh:
+	min1 = jsmin(content)
+	min2 = "// Rajs.min.js, a small library for everything. check out https://github.com/oboforty/Rajs\n" + min1.replace("\n", "")
+	fh.write(min2)
