5e520064a021642e2d42661c64f900806db1c8a3
Rajmund <r.csombordi@fugro.com>
2018-04-18 23:23:25 +0200

    Added params.set

--- primitives/cookieMonster.js
+++ primitives/cookieMonster.js
@@ -39,5 +39,28 @@ var Params = {
         if (!this.inited) {this.init(); this.inited=true;}
         return this.params[name];
     },
+
+    set: function(key, value) {
+        key = encodeURI(key); value = encodeURI(value);
+
+        var kvp = document.location.search.substr(1).split('&');
+
+        var i=kvp.length; var x; while(i--) 
+        {
+            x = kvp[i].split('=');
+
+            if (x[0]==key)
+            {
+                x[1] = value;
+                kvp[i] = x.join('=');
+                break;
+            }
+        }
+
+        if(i<0) {kvp[kvp.length] = [key,value].join('=');}
+
+        //this will reload the page, it's likely better to store this until finished
+        document.location.search = kvp.join('&'); 
+    }
 };
 Params.init();
