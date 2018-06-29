c9aa11067e3b94cc60e80d61182b247df7762c50
r.csombordi <r.csombordi@fugro.com>
2018-06-29 11:16:11 +0200

    Added 2nd parameter to Math.log

--- src/math.js
+++ src/math.js
@@ -2,3 +2,10 @@
 Math.rad2deg = 180/Math.PI;
 Math.deg2rad = Math.PI/180;
 Math.TAU = 2*Math.PI;
+
+Math.log = (function() {
+  var log = Math.log;
+  return function(n, base) {
+    return log(n)/(base ? log(base) : 1);
+  };
+})();
