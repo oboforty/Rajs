81455906034441364e9e33cae38b59a7c8ee58ca
oboforty <rajmund.csombordi@hotmail.com>
2018-07-08 00:39:05 +0200

    Added type callables and defaultdict

--- src/pythonic.js
+++ src/pythonic.js
@@ -59,3 +59,73 @@ String.prototype.lower = function() {
 String.prototype.upper = function() {
   return this.toUpperCase();
 };
+
+
+function str(val) {
+  if (typeof val === 'undefined')
+    return '';
+  if (typeof val === 'string')
+    return val;
+  return val.toString();
+}
+
+function float(val) {
+  if (typeof val === 'undefined')
+    return 0.0;
+  if (typeof val === 'string')
+    return parseFloat(val);
+  if (typeof val === 'number')
+    return val;
+}
+
+function int(val) {
+  if (typeof val === 'undefined')
+    return 0;
+  if (typeof val === 'string')
+    return parseInt(val);
+  if (typeof val === 'number')
+    return val;
+}
+
+function bool(val) {
+  if (typeof val === 'undefined')
+    return false;
+  return Boolean(val);
+}
+
+function dict(val) {
+  if (typeof val === 'undefined')
+    return {};
+
+  var v = {};
+  for (var key in val) {
+    var e = val[key];
+    v[e[0]] = e[1];
+  }
+  return v;
+}
+
+function list(val) {
+  if (typeof val === 'undefined')
+    return [];
+  var l = [];
+  for (var elem of val) {
+    l.push(elem);
+  }
+  return l;
+}
+
+function defaultdict(deftype, isFunc) {
+  return new Proxy({}, {
+    get: function(target, name) {
+      if (typeof target[name] !== 'undefined')
+        return target[name];
+      else if (typeof deftype === 'function') {
+        if (isFunc)
+          return deftype();
+        else
+          return new deftype();
+      }
+    }
+  });
+}
